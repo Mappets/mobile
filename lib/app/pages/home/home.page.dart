6ad67cd6b2136fc7e560c766b10a60fdbec28cec
Matheus Martins <matheusprotos@gmail.com>
2020-08-09 13:41:27 -0300

    Change controller class name.

--- lib/app/pages/home/home.page.dart
+++ lib/app/pages/home/home.page.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
-import 'package:get/get.dart';
-import 'package:mobile/app/pages/home/home.controller.dart';
 
 class HomePage extends StatelessWidget {
-  final Controller controller = Get.put(Controller());
-
   @override
   Widget build(context) => Scaffold(
       appBar: AppBar(title: Text("Home")),
